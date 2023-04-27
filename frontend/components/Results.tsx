69d076fc8e7429930f142b64901f338ce69cabb1
Keenan Graham <ksgraham@stanford.edu>
2023-04-26 20:44:24 -0700

    Add lab and link

--- frontend/components/Results.tsx
+++ frontend/components/Results.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Link from "next/link"
+
 
 type Result = {
     id: string;
@@ -31,12 +33,18 @@ function Results(props: ResultsProps) {
 
             {
                 props.rawResults?.map(
-                    result => (
-                        <div key={result.id} className="w-full shadow-2xl drop-shadow-lg rounded-sm p-10 bg-gray-50">
+                    (result, i: number) => (
+                        <div
+                            key={result.id}
+                            className="w-full shadow-2xl drop-shadow-lg rounded-sm p-10 bg-gray-50 hover:bg-violet-50"
+                        >
                             <div className="flex flex-col gap-y-2">
-                                <div>ID: <span>{result.id}</span></div>
+                                <div>
+                                    ID: <Link className="underline" href={`https://www.encodeproject.org${result.id}`} rel="noopener noreferrer" target="_blank">{result.id}</Link>
+                                </div>
                                 {result.document.title && <div>Title: <span className="font-bold">{result.document.title}</span></div>}
                                 <div>Score: <span className="text-violet-500 text-bold">{result.score.toFixed(7)}</span></div>
+                                {result.document.pi && <div>Lab: <span className="font-light">{result.document.pi.title}</span></div>}
                                 {result.document.description && <div>Description: <span className="font-light">{result.document.description}</span></div>}
                             </div>
                         </div>
